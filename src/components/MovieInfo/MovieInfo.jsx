dadc315f8871a7663850b5a4600befb81678320c
Lorraine Martin <lolocodes2001@gmail.com>
2023-10-04 14:49:45 -0700

    added background colors, box container sizes

--- src/components/MovieInfo/MovieInfo.jsx
+++ src/components/MovieInfo/MovieInfo.jsx
@@ -1,16 +1,15 @@
 const MovieInfo = ({ movieObj }) => {
-  console.log(movieObj);
   return (
-    <div>
+    <div className="flex-item">
       <h4>{movieObj.title}</h4>
       <div>
         <div>
           <span>Running Time</span>
-          <span></span>
+          <span>{movieObj.runningTime}</span>
         </div>
         <div>
           <span>Genre</span>
-          <span></span>
+          <span>{movieObj.genre}</span>
         </div>
       </div>
     </div>
