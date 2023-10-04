304502dfddf6871d3fa855a20ce5d30bd5d4fd67
Lorraine Martin <lolocodes2001@gmail.com>
2023-10-04 14:32:10 -0700

    rendered objects with dot notation for running time and genre

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import Header from "./components/Header/Header";
 import MovieList from "./components/MovieList/MovieList";
 import MovieInfo from "./components/MovieInfo/MovieInfo";
 function App() {
+  const selectedMovie = {
+    title: "Forrest Gump",
+    runningTime: 142,
+    genre: "Romantic Drama",
+  };
+
   return (
     <div className="App">
       <Header />
       <MovieList />
-      <MovieInfo
-        title="Forrest Gump"
-        runningTime={142}
-        genre="Romantic Drama"
-      />
+      <MovieInfo movieObj={selectedMovie} />
     </div>
   );
 }
